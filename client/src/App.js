af86d8f24a598c284011fac6ef255a6dc0bf699e
Tejas Parab <tejasparab890@gmail.com>
2018-07-12 20:39:30 -0400

    basic registration and login setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
-
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path = "/" component={ Landing } />
+          <div className="container">
+            <Route exact path = "/register" component= {  Register  } />
+            <Route exact path = "/login" component= {  Login  } />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
