3d8289a51b70e5d3cd3adafcb46dd709de047845
Tejas Parab <tejasparab890@gmail.com>
2018-09-03 05:23:46 -0400

    create profile submitting and working.

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -5,6 +5,8 @@ import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import InputGroup from '../common/InputGroup';
 import SelectListGroup from '../common/SelectListGroup';
+import {createProfile} from '../../actions/profileActions';
+import {withRouter } from 'react-router-dom';
 
 class CreateProfile extends Component {
   constructor(props) {
@@ -30,9 +32,31 @@ class CreateProfile extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.errors) {
+      this.setState({errors: nextProps.errors});
+    }
+  }
+
   onSubmit(e) {
     e.preventDefault();
-    console.log('submit');
+
+    const profileData = {
+      handle: this.state.handle,
+      company: this.state.company,
+      website: this.state.website,
+      location: this.state.location,
+      status: this.state.status,
+      skills: this.state.skills,
+      githubusername: this.state.githubusername,
+      bio: this.state.bio,
+      twitter: this.state.twitter,
+      facebook: this.state.facebook,
+      linkedin: this.state.linkedin,
+      youtube: this.state.youtube,
+      instagram: this.state.instagram
+    }
+    this.props.createProfile(profileData, this.props.history);
   }
 
   onChange(e) {
@@ -181,11 +205,13 @@ class CreateProfile extends Component {
                   info = "Tell us a little about yourself"
                 />
                 <div className = "mb-3">
-                  <button onClick = {() => {
-                    this.setState(prevState => ({
-                      displaySocialInputs: !prevState.displaySocialInputs
-                    }))
-                  }} className="btn btn-light">
+                  <button
+                    type = "button"
+                    onClick = {() => {
+                      this.setState(prevState => ({
+                        displaySocialInputs: !prevState.displaySocialInputs
+                      }))
+                    }} className="btn btn-light">
                     Add Social Network Links
                   </button>
                   <span className = "text-muted">Optional</span>
@@ -214,4 +240,4 @@ const mapStateToProps = state => ({
 })
 
 
-export default connect(mapStateToProps)(CreateProfile);
+export default connect(mapStateToProps, {createProfile })(withRouter(CreateProfile));
