45ef55135f9df35e5da7267bcbf2c4f9deec1a2e
Tejas Parab <tejasparab890@gmail.com>
2018-09-04 19:03:49 -0400

    posts add and delete working for authorized user

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import NotFound from './components/not-found/NotFound';
+import Posts from './components/posts/Posts';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
@@ -73,6 +74,9 @@ class App extends Component {
               </Switch>
               <Switch>
                 <PrivateRoute exact path = "/add-education" component = { AddEducation } />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path = "/feed" component = { Posts } />
               </Switch>
               <Route exact path = "/not-found" component= {  NotFound  } />
             </div>
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import errorReducer from './errorReducer';
 import profileReducer from './profileReducer';
+import postReducer from './postReducer';
 // this is the root reducer, we bring in all of the other reducers here.
 export default combineReducers({
   auth: authReducer,
   errors: errorReducer,
-  profile: profileReducer
+  profile: profileReducer,
+  post: postReducer
 });
