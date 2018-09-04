333ee5fa26f20cd891dd454b56096272c2c4ff02
Tejas Parab <tejasparab890@gmail.com>
2018-09-03 23:20:59 -0400

    edit profile working

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -17,7 +17,11 @@ class Navbar extends Component {
     const { isAuthenticated, user } = this.props.auth;
     const authLinks = (
       <ul className="navbar-nav ml-auto">
-
+        <li className = "nav-item">
+          <Link className = "nav-link" to="/dashboard">
+            Dashboard
+          </Link>
+        </li>
         <li className="nav-item">
           <a href="" onClick = {this.onLogoutClick.bind(this)} className = "nav-link">
             <img className = "rounded-circle" src={user.avatar} alt={user.name} style = {{width: '25px', marginRight: '5px'}} title="you must have a gravatar connected
