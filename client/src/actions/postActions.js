1d6d885fd3f6c2b6a758b0f928f4e0cf4ce80805
Tejas Parab <tejasparab890@gmail.com>
2018-09-04 19:26:21 -0400

    like/unlike working

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,28 +1,25 @@
 import Axios from 'axios';
 
 
-import {
-  ADD_POST,
-  GET_ERRORS, GET_POSTS, POST_LOADING
-} from './types';
+import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING, DELETE_POST} from './types';
 
 
 //ADD POST
 export const addPost = postData => dispatch => {
   Axios
-    .post('/api/posts', postData)
-    .then(res =>
-      dispatch({
-        type: ADD_POST,
-        payload: res.data
-      })
-    )
-    .catch(err =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
+  .post('/api/posts', postData)
+  .then(res =>
+    dispatch({
+      type: ADD_POST,
+      payload: res.data
+    })
+  )
+  .catch(err =>
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    })
+  );
 };
 
 
@@ -30,19 +27,66 @@ export const addPost = postData => dispatch => {
 export const getPosts = () => dispatch => {
   dispatch(setPostLoading());
   Axios
-    .get('/api/posts')
-    .then(res =>
-      dispatch({
-        type: GET_POSTS,
-        payload: res.data
-      })
-    )
-    .catch(err =>
-      dispatch({
-        type: GET_POSTS,
-        payload: null
-      })
-    );
+  .get('/api/posts')
+  .then(res =>
+    dispatch({
+      type: GET_POSTS,
+      payload: res.data
+    })
+  )
+  .catch(err =>
+    dispatch({
+      type: GET_POSTS,
+      payload: null
+    })
+  );
+};
+
+
+// Delete post
+export const deletePost = id => dispatch => {
+  Axios
+  .delete(`/api/posts/${id}`)
+  .then(res =>
+    dispatch({
+      type: DELETE_POST,
+      payload: id
+    })
+  )
+  .catch(err =>
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    })
+  );
+};
+
+// Add like
+export const addLike = id => dispatch => {
+  Axios
+  .post(`/api/posts/like/${id}`)
+  .then(res =>
+    dispatch(getPosts()))
+  .catch(err =>
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    })
+  );
+};
+
+// Remove like
+export const removeLike = id => dispatch => {
+  Axios
+  .post(`/api/posts/unlike/${id}`)
+  .then(res =>
+    dispatch(getPosts()))
+  .catch(err =>
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    })
+  );
 };
 
 // set loading state
